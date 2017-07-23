e4aeda97140aec63b43a69e180b8e54a6607ffe6
Nazih Benoumechiara <nazih.benoumechiara@gmail.com>
2017-07-24 00:45:31 +0200

    Start of shappley class and init methods.

--- shapley/python/shapley.py
+++ shapley/python/shapley.py
@@ -1,7 +1,29 @@
 import numpy as np
+import itertools
 
 class Shapley(object):
     """Shappley indices object estimator
     """
-    def __init__(self, model):
+    def __init__(self, model, dim):
         self.model = model
+        self.dim = dim
+
+    def compute_indices(self):
+        """
+        """
+        dim = self.dim
+        perms = itertool.permutations(range(dim), dim)
+        n_perms = len(perms)
+
+
+    @property
+    def dim(self):
+        """
+        """
+        return self._dim
+
+    @dim.setter
+    def dim(self, dim):
+        assert dim > 0
+        self._dim = dim
+        self._dim = dim
