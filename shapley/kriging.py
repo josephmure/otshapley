719654fa3cc24809dc71a82c56a881b44d5ac039
Benoumechiara Nazih <nazih.benoumechiara@gmail.com>
2017-07-25 12:33:44 +0200

    autosave

--- shapley/kriging.py
+++ shapley/kriging.py
@@ -26,6 +26,11 @@ class KrigingIndices(object):
         kriging_algo = ot.KrigingAlgorithm(input_sample, output_sample, covariance, basis)
         kriging_algo.run()
         self.kriging_result = kriging_algo.getResult()
+
+    def kriging_function(self, X):
+        """
+        """
+        return ot.KrigingRandomVector(self.kriging_result, X)
         
     def compute_indices(self, n_sample=200, n_realization=10, n_bootstrap=50, with_second_order=False):
         """Compute the indices.
@@ -42,25 +47,32 @@ class KrigingIndices(object):
         input_sample_1 = np.asarray(self.input_distribution.getSample(n_sample))
         input_sample_2 = np.asarray(self.input_distribution.getSample(n_sample))
 
-        input_design = np.asarray(ot.SobolIndicesAlgorithmImplementation.Generate(self.input_distribution, n_sample, with_second_order))
-        kriging_vector = ot.KrigingRandomVector(self.kriging_result, input_design)
-        output_designs = np.asarray(kriging_vector.getSample(n_realization)).T
-        
-        first_indices = np.zeros((self.dim, n_realization, n_bootstrap))
-        n_design = input_design.shape[0]
-        for i_nz in range(n_realization):
-            output_design = output_designs[:, i_nz].reshape(-1, 1)
-            sensitivity_normal = ot.SaltelliSensitivityAlgorithm(input_design, output_design, n_sample)
-            first_indices[:, i_nz, 0] = np.asarray(sensitivity_normal.getFirstOrderIndices())
-            for i_b in range(1, n_bootstrap):
-                # Bootstrap of design space
-                boot_idx = np.random.choice(range(n_design), n_design)
-                input_design_boot = input_design[boot_idx]
-                output_design_boot = output_design[boot_idx]
+        for i in range(self.dim):
+            X = input_sample_1
+            Xt = input_sample_2.copy()
+            Xt[:, i] = input_sample_1[:, i]
+            input_design = np.r_[X, Xt]
+            output_design = np.r_[Y, Xt]
+            indices[i] = janon_estimator(self.kriging_function, X, Xt)
 
-                # Compute the indices
-                sensitivity_boot = ot.SaltelliSensitivityAlgorithm(input_design_boot, output_design_boot, n_sample)
-                first_indices[:, i_nz, i_b] = np.asarray(sensitivity_boot.getFirstOrderIndices())
+            kriging_vector = self.kriging_function(input_design)
+            output_designs = np.asarray(kriging_vector.getSample(n_realization)).T
+        
+            first_indices = np.zeros((self.dim, n_realization, n_bootstrap))
+            n_design = input_design.shape[0]
+            for i_nz in range(n_realization):
+                output_design = output_designs[:, i_nz].reshape(-1, 1)
+                sensitivity_normal = ot.SaltelliSensitivityAlgorithm(input_design, output_design, n_sample)
+                first_indices[:, i_nz, 0] = np.asarray(sensitivity_normal.getFirstOrderIndices())
+                for i_b in range(1, n_bootstrap):
+                    # Bootstrap of design space
+                    boot_idx = np.random.choice(range(n_design), n_design)
+                    input_design_boot = input_design[boot_idx]
+                    output_design_boot = output_design[boot_idx]
+
+                    # Compute the indices
+                    sensitivity_boot = ot.SaltelliSensitivityAlgorithm(input_design_boot, output_design_boot, n_sample)
+                    first_indices[:, i_nz, i_b] = np.asarray(sensitivity_boot.getFirstOrderIndices())
                 
         return first_indices
 
