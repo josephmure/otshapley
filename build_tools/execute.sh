f9cf3b18d0d7c45fd540497aebb1acb1da8a6828
Benoumechiara Nazih <nazih.benoumechiara@gmail.com>
2017-07-31 14:55:25 +0200

    Another correction

--- build_tools/execute.sh
+++ build_tools/execute.sh
@@ -3,7 +3,8 @@ export PATH="$HOME/miniconda/bin:$PATH"
 source activate testenv
 
 if [ ${COVERAGE} == "true" ]; then
-	nosetests -c .noserc -q --cover-html-dir=build --cover-html
+	mkdir build
+	nosetests --with-coverage -q --cover-html-dir=build --cover-html
 	coverage report -m
 else
     nosetests -v -sx
