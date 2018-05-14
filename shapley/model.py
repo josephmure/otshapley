9e429b4b1aabbb4e3721b8021a8e4b48cfce56f8
Benoumechiara Nazih <nazih.benoumechiara@gmail.com>
2018-05-14 19:41:49 +0200

    Corrections

--- shapley/model.py
+++ shapley/model.py
@@ -2,21 +2,23 @@ import numpy as np
 import openturns as ot
 import pandas as pd
 
-from .utils import q2_cv, DF_NAMES
+from .utils import DF_NAMES, q2_cv
 
-# TODO: parallelize the model function
 
 class Model(object):
     """A model that evaluate a given function.
 
-    This class aims to gather the informations of a function and eventually 
-    parallelize it.
+    This class aims to gather the informations of a model function.
 
     Parameters
     ----------
-    model_func : callable,
+    model_func : callable
         The model function.
+
+    name : str
+        The function name.
     """
+
     def __init__(self, model_func, name='Custom'):
         self.model_func = model_func
         self.name = name
@@ -52,6 +54,9 @@ class ProbabilisticModel(Model):
     input_distribution : ot.DistributionImplementation
         The probabilistic input distribution.
 
+    name : str, optional (default='Custom')
+        The function name.
+
     first_sobol_indices : list, array or None, optional (default=None)
         The true first-order sobol indices.
 
@@ -61,17 +66,15 @@ class ProbabilisticModel(Model):
     shapley_indices : list, array or None, optional (default=None)
         The true shapley indices.
     """
-    def __init__(self, 
-                 model_func, 
-                 input_distribution,
+
+    def __init__(self,
+                 model_func,
+                 input_distribution=None,
                  name='Custom',
                  first_sobol_indices=None,
                  total_sobol_indices=None,
                  shapley_indices=None):
-        #super(ProbabilisticModel, self).__init__(
-        #    model_func=model_func)
         Model.__init__(self, model_func=model_func, name=name)
-
         self.input_distribution = input_distribution
         self.first_sobol_indices = first_sobol_indices
         self.total_sobol_indices = total_sobol_indices
@@ -99,14 +102,12 @@ class ProbabilisticModel(Model):
     @property
     def first_sobol_indices(self):
         """The true first-order sobol indices.
-        """        
+        """
         return self._first_sobol_indices
 
     @first_sobol_indices.setter
     def first_sobol_indices(self, indices):
-        if indices is not None:
-            assert len(indices) == self._dim, \
-                "Incorrect number of indices: %d!=%d" % (len(indices), self._dim)
+        check_indices(indices, self._dim)
         self._first_sobol_indices = indices
 
     @property
@@ -117,9 +118,7 @@ class ProbabilisticModel(Model):
 
     @total_sobol_indices.setter
     def total_sobol_indices(self, indices):
-        if indices is not None:
-            assert len(indices) == self._dim, \
-                "Incorrect number of indices: %d!=%d" % (len(indices), self._dim)
+        check_indices(indices, self._dim)
         self._total_sobol_indices = indices
 
     @property
@@ -130,40 +129,33 @@ class ProbabilisticModel(Model):
 
     @shapley_indices.setter
     def shapley_indices(self, indices):
-        if indices is not None:
-            assert len(indices) == self._dim, \
-                "Incorrect number of indices: %d!=%d" % (len(indices), self._dim)
+        check_indices(indices, self._dim)
         self._shapley_indices = indices
 
     @property
     def margins(self):
-        """The problem margins.
+        """The input marginal distribution.
         """
         return self._margins
 
     @margins.setter
     def margins(self, margins):
-        for marginal in margins:
-            assert isinstance(marginal, ot.DistributionImplementation), \
-                "The marginal should be an OpenTURNS implementation."
-        assert len(margins) == self._dim, \
-            "Incorrect dimension: %d!=%d" % (len(margins), self._dim)
-        self._input_distribution = ot.ComposedDistribution(margins, self._copula)
+        check_margins(margins, self._dim)
+        self._input_distribution = ot.ComposedDistribution(
+            margins, self._copula)
         self._margins = margins
 
     @property
     def copula(self):
-        """The input distribution copula.
+        """The input copula.
         """
         return self._copula
-    
+
     @copula.setter
     def copula(self, copula):
-        assert isinstance(copula, (ot.CopulaImplementation, ot.DistributionImplementationPointer)), \
-            "The copula should be an OpenTURNS implementation: {0}".format(type(copula))
-        assert copula.getDimension() == self._dim, \
-            "Incorrect dimension: %d!=%d" % (copula.getDimension(), self._dim)
-        self._input_distribution = ot.ComposedDistribution(self._margins, copula)
+        check_copula(copula, self._dim)
+        self._input_distribution = ot.ComposedDistribution(
+            self._margins, copula)
         self._copula = copula
 
     @property
@@ -177,6 +169,7 @@ class ProbabilisticModel(Model):
         copula = self._copula
         copula.setParameter(params)
         self.copula = copula
+        self._copula_parameters = params
 
     @property
     def dim(self):
@@ -202,7 +195,7 @@ class ProbabilisticModel(Model):
         """
         assert isinstance(n_sample, int), "Sampling number must be an integer"
         assert isinstance(sampling, str), "Sampling must be a string"
-        
+
         dist = self._input_distribution
         input_sample = sample_dist(dist, n_sample, sampling)
 
@@ -211,7 +204,7 @@ class ProbabilisticModel(Model):
     @property
     def df_indices(self):
         """A dataframe of the true indices.
-        
+
         Returns
         -------
         indices : dataframe
@@ -226,9 +219,11 @@ class ProbabilisticModel(Model):
             d_indices['True shapley'] = self._shapley_indices
 
         if d_indices:
-            d_indices[DF_NAMES['var']] = ['$X_{%d}$' % (i+1) for i in range(self._dim)]
+            d_indices[DF_NAMES['var']] = [
+                '$X_{%d}$' % (i+1) for i in range(self._dim)]
             df = pd.DataFrame(d_indices)
-            indices = pd.melt(df, id_vars=[DF_NAMES['var']], var_name=DF_NAMES['ind'], value_name=DF_NAMES['val'])
+            indices = pd.melt(df, id_vars=[
+                              DF_NAMES['var']], var_name=DF_NAMES['ind'], value_name=DF_NAMES['val'])
         else:
             indices = None
             print('There is no true indices.')
@@ -247,7 +242,11 @@ class MetaModel(ProbabilisticModel):
 
     input_distribution : ot.DistributionImplementation or None, optional (default=None)
         The probabilistic input distribution.
+
+    name : str, optional (default='Custom')
+        The function name.
     """
+
     def __init__(self, model=None, input_distribution=None, name='Custom'):
         if isinstance(model, ProbabilisticModel):
             super(MetaModel, self).__init__(
@@ -257,7 +256,7 @@ class MetaModel(ProbabilisticModel):
                 first_sobol_indices=model.first_sobol_indices,
                 total_sobol_indices=model.total_sobol_indices,
                 shapley_indices=model.shapley_indices
-                )
+            )
         else:
             super(MetaModel, self).__init__(
                 model_func=None,
@@ -265,20 +264,24 @@ class MetaModel(ProbabilisticModel):
                 name=name)
         self.true_model = model
 
-    def generate_sample(self, n_sample=50, sampling='lhs', sampling_type='uniform', alpha=0.999):
+    def generate_sample(self, n_sample, sampling='lhs', sampling_type='uniform', alpha=0.999):
         """Generate the sample to build the model.
 
         Parameters
         ----------
-        n_sample : int,
+        n_sample : int
             The sampling size.
-        sampling : str,
-            The sampling method to use.
-        """
-        dist = change_distribution(self._input_distribution, sampling_type, alpha)
-        input_sample = sample_dist(dist, n_sample, sampling)
 
-        self.input_sample = np.asarray(input_sample)
+        sampling : str, optional (default='lhs')
+            The sampling method to use.
+
+        sampling_type : str, optional (default='uniform')
+            The type of sampling.
+        """
+        dist = change_distribution(
+            self._input_distribution, sampling_type, alpha)
+        input_sample = sample_dist(dist, n_sample, sampling)
+        self.input_sample = input_sample
         self.output_sample = self.true_model(input_sample)
 
     @property
@@ -286,11 +289,12 @@ class MetaModel(ProbabilisticModel):
         """The input sample to build the model.
         """
         return self._input_sample
-    
+
     @input_sample.setter
     def input_sample(self, sample):
         n_sample, dim = sample.shape
-        assert dim == self._dim, "Dimension should be the same as the input_distribution: %d != %d" % (dim, self._dim)
+        assert dim == self._dim, "Dimension should be the same as the input_distribution: %d != %d" % (
+            dim, self._dim)
         self._n_sample = n_sample
         self._input_sample = sample
 
@@ -299,19 +303,21 @@ class MetaModel(ProbabilisticModel):
         """The output sample to build the model.
         """
         return self._output_sample
-    
+
     @output_sample.setter
     def output_sample(self, sample):
         n_sample = sample.shape[0]
-        assert n_sample == self._n_sample, "Samples should be the same sizes: %d != %d" % (n_sample, self._n_samples)
+        assert n_sample == self._n_sample, "Samples should be the same sizes: %d != %d" % (
+            n_sample, self._n_sample)
         self._output_sample = sample
-        
+
     def compute_score_q2_cv(self, n_sample=100, sampling='lhs', sampling_type='classic', alpha=0.99):
         """Cross Validation estimation of Q2.
-        """        
-        dist = change_distribution(self._input_distribution, sampling_type, alpha)
+        """
+        dist = change_distribution(
+            self._input_distribution, sampling_type, alpha)
         x = sample_dist(dist, n_sample, sampling)
-        
+
         ytrue = self.true_model(x)
         ypred = self.predict(x)
         q2 = q2_cv(ytrue, ypred)
@@ -319,9 +325,13 @@ class MetaModel(ProbabilisticModel):
         return q2
 
     def __call__(self, X, n_estimators):
-        y = self._model_func(X, n_estimators)
+        if n_estimators == 1:
+            y = self.predict(X)
+        else:
+            y = self._model_func(X, n_estimators)
         return y
-    
+
+
 def get_margins(dist):
     """
     """
@@ -330,6 +340,7 @@ def get_margins(dist):
     for i in range(dim):
         margins.append(dist.getMarginal(i))
     return margins
+
 
 def sample_dist(dist, n_sample, sampling):
     """
@@ -344,12 +355,13 @@ def sample_dist(dist, n_sample, sampling):
 
     return np.asarray(input_sample)
 
+
 def change_distribution(dist, sampling_type, alpha):
     """Slightly change the distribution for a custom sampling.
-    
+
     Parameters
     ----------
-    
+
     """
     dist = ot.ComposedDistribution(dist)
     if sampling_type == 'uniform':
@@ -367,5 +379,28 @@ def change_distribution(dist, sampling_type, alpha):
         pass
     else:
         raise ValueError('Unknow sampling type')
-        
+
     return dist
+
+
+def check_indices(indices, dim):
+    if indices is not None:
+        assert len(indices) == dim, \
+            "Incorrect number of indices: %d!=%d" % (
+                len(indices), dim)
+
+
+def check_margins(margins, dim):
+    for marginal in margins:
+        assert isinstance(marginal, ot.DistributionImplementation), \
+            "The marginal should be an OpenTURNS implementation."
+    assert len(margins) == dim, \
+        "Incorrect dimension: %d!=%d" % (len(margins), dim)
+
+
+def check_copula(copula, dim):
+    assert isinstance(copula, (ot.CopulaImplementation, ot.DistributionImplementationPointer)), \
+        "The copula should be an OpenTURNS implementation: {}".format(
+            type(copula))
+    assert copula.getDimension() == dim, \
+        "Incorrect dimension: %d!=%d" % (copula.getDimension(), dim)
